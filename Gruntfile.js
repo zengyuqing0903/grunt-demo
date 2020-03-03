1f2bd02c7e6b1c16b10c9b2ca44c5c674af48ea2
zengyuqing0903 <3372284665@qq.com>
2020-03-03 20:31:49 +0800

    前端单元测试

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,17 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    mochacli: {
-      options: {
-        reporter: 'spec',
-        bail: true
+    mocha: {
+      test: {
+        src: ['test/index.html'],
       },
-      all: ['test/*.js']
-    },
-    mocha_istanbul: {
-      coverage: {
-        src: 'test'
-      }
-    },
-    istanbul_check_coverage: {
-      default: {
-        options: {
-          coverageFolder: 'coverage*',
-          check: {
-            lines: 90,
-            statements: 90
-          }
-        }
+      options: {
+        run: true,
+        reporter: 'Dot'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-mocha');
 
-  grunt.registerTask('default', ['mochacli']);   
-  grunt.registerTask('cover', ['mocha_istanbul']);
-  grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+  grunt.registerTask('default', ['mocha']);
 };
