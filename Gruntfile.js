bdec9066d0aafbb06b5d937a5a711c592585287b
zengyuqing0903 <3372284665@qq.com>
2020-03-04 12:11:33 +0800

    http接口测试

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,22 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    mocha: {
-      test: {
-        src: ['test/index.html'],
-      },
+    run: {
+      api: {
+        options: {wait:false},
+        args: ['./app.js']
+      }
+    },
+    mochacli: {
+      all: ['test/*.js'],
       options: {
-        run: true,
-        reporter: 'Dot'
+        reporter: 'spec',
+        bail: true
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-run');
 
-  grunt.registerTask('default', ['mocha']);
+  grunt.registerTask('default', ['run','mochacli','stop:api']);
 };
