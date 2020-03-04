0638e861034ddeb26e0c6b1f1b9edde8a5bfd253
zengyuqing0903 <3372284665@qq.com>
2020-03-04 20:21:58 +0800

    html代码压缩htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,19 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    run: {
-      api: {
-        options: {wait:false},
-        args: ['./app.js']
-      }
-    },
-    mochacli: {
-      all: ['test/*.js'],
-      options: {
-        reporter: 'spec',
-        bail: true
+    htmlmin: {
+      options:{
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
       }
     }
+
   });
 
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-run');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  grunt.registerTask('default', ['run','mochacli','stop:api']);
+  grunt.registerTask('default', ['htmlmin']);
 };
